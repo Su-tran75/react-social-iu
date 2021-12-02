32ae17369a52a93af7d861e579018cf580206677
su-tran75 <su-tran@hotmail.fr>
2021-12-02 18:01:06 +0800

    Profile Page

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -3,10 +3,10 @@ import "./rightBar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-export default function RightBar() {
-  return (
-    <div className="rightBar">
-      <div className="rightBarWrapper">
+export default function RightBar({ profile }) {
+  const HomeRightBar = () => {
+    return (
+      <>
         <div className="birthdayContainer">
           <img src="assets/gift.png" className="birthdayImg" alt="" />
           <span className="birthdayText">
@@ -20,6 +20,95 @@ export default function RightBar() {
             <Online key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    );
+  };
+
+  const ProfileRightBar = () => {
+    return (
+      <>
+        <h4 className="rightBarTitle">User Information</h4>
+        <div className="rightBarInfo">
+          <div className="rightBarInfoItem">
+            <span className="rightBarInfoKey">City:</span>
+            <span className="rightBarValue">Barcelona</span>
+          </div>
+          <div className="rightBarInfoItem">
+            <span className="rightBarInfoKey">From:</span>
+            <span className="rightBarValue">Paris</span>
+          </div>
+          <div className="rightBarInfoItem">
+            <span className="rightBarInfoKey">Relationship:</span>
+            <span className="rightBarValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightBarTitle">User friends</h4>
+        <div className="rightBarFollowings">
+          <div className="rightBarFollowing">
+            <img
+              src="assets/person/7.jpeg"
+              className="rightBarFollowingImg"
+              alt=""
+            />
+            <span className="rightBarFollowingName">John Doe</span>
+          </div>
+          <div className="rightBarFollowing">
+            <img
+              src="assets/person/7.jpeg"
+              className="rightBarFollowingImg"
+              alt=""
+            />
+            <span className="rightBarFollowingName">John Doe</span>
+          </div>
+          <div className="rightBarFollowing">
+            <img
+              src="assets/person/10.jpeg"
+              className="rightBarFollowingImg"
+              alt=""
+            />
+            <span className="rightBarFollowingName">John Doe</span>
+          </div>
+          <div className="rightBarFollowing">
+            <img
+              src="assets/person/9.jpeg"
+              className="rightBarFollowingImg"
+              alt=""
+            />
+            <span className="rightBarFollowingName">John Doe</span>
+          </div>
+          <div className="rightBarFollowing">
+            <img
+              src="assets/person/8.jpeg"
+              className="rightBarFollowingImg"
+              alt=""
+            />
+            <span className="rightBarFollowingName">John Doe</span>
+          </div>
+          <div className="rightBarFollowing">
+            <img
+              src="assets/person/6.jpeg"
+              className="rightBarFollowingImg"
+              alt=""
+            />
+            <span className="rightBarFollowingName">John Doe</span>
+          </div>
+          <div className="rightBarFollowing">
+            <img
+              src="assets/person/7.jpeg"
+              className="rightBarFollowingImg"
+              alt=""
+            />
+            <span className="rightBarFollowingName">John Doe</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightBar">
+      <div className="rightBarWrapper">
+        <ProfileRightBar />
       </div>
     </div>
   );
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./profile.css";
+import TopBar from "../../components/topBar/TopBar";
+import SideBar from "../../components/sideBar/SideBar";
+import Feed from "../../components/feed/Feed";
+import RightBar from "../../components/rightBar/RightBar";
+
+export default function Profile() {
+  return (
+    <>
+      <TopBar />
+      <div className="profile">
+        <SideBar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/5.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Leo Messi</h4>
+              <h4 className="profileInfoDesc">Hello my friend.</h4>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <RightBar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
