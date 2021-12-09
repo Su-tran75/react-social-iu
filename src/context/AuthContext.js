a10e5ca6fef669a7217441981388f070b97315f5
su-tran75 <su-tran@hotmail.fr>
2021-12-09 12:37:30 +0800

    Temporary state to avoid log in when refresh

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,7 +2,24 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user: null,
+  user: {
+    _id: "61a5db664f7346581d689b1d",
+    username: "leo",
+    email: "leo@gmail.com",
+    password: "$2b$10$X0MqpZ4qkYvw5sDqOMvEQOAhYKGJilS/Qmja6B5FgEaCrg3ioPbiK",
+    profilePicture: "person/10.jpeg",
+    coverPicture: "",
+    followers: ["61a5d81160f9aa22a5366423"],
+    isAdmin: false,
+    createdAt: { $date: { $numberLong: "1638259558213" } },
+    updatedAt: { $date: { $numberLong: "1638267200138" } },
+    __v: { $numberInt: "0" },
+    followings: ["61a5d81160f9aa22a5366423"],
+    desc: "The Goat",
+    city: "Paris",
+    from: "Barcelona",
+    relationship: "2",
+  },
   isFetching: false,
   error: false,
 };
